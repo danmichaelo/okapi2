88232faf78cb6c48246eccd3dcf7047e4654ed60
Dan Michael O. Heggø <danmichaelo@gmail.com>
2017-06-07 19:50:59 +0200

    Add authority model

---
+++ common/models/authority.js
@@ -0,0 +1,55 @@
+'use strict';
+
+module.exports = function(Authority) {
+
+  Authority.complete = function (q, cb) {
+    var connector = Authority.dataSource.connector
+    var modelName = 'authority';
+    var idName = connector.idName(modelName)
+
+    if (!q) {
+      cb(null, [])
+      return;
+    }
+
+    console.log('Complete: "' + q + '" idname: ' + idName)
+    connector.db.search({
+      index: 'authority',
+      type: 'record',
+      body: {
+        suggest: {
+          'term' : {
+            prefix : q,
+            completion : {
+              field : 'suggest'
+            }
+          }
+        }
+      }
+    }, function (error, response) {
+      if (error) console.error(error);
+
+      console.log('Got data')
+      if (!response.suggest) {
+        cb(error, []);
+      }
+      console.log(response.suggest.term)
+
+      var results = [];
+      if (response.hits) {
+        response.suggest.term[0].options.forEach(function (item) {
+          results.push(item._source);
+        });
+      }
+      cb(error, results);
+    });
+  };
+
+  Authority.remoteMethod('complete', {
+    http: { verb: 'get' },
+    description: 'ElasticSearch auto-completion',
+    accepts: {arg: 'q', type: 'string'},
+    returns: {arg: 'data', type: ['object'], root: true}
+  });
+
+};
