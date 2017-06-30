b3eba7f87633a51e35acc9393440de67a61ef680
Dan Michael O. Heggø <danmichaelo@gmail.com>
2017-06-30 03:26:10 +0200

    Add support for the ES scroll api

--- common/models/biblio.js
+++ common/models/biblio.js
@@ -6,19 +6,25 @@ module.exports = function(Biblio) {
    * Search using ElasticSearch Query String Query instead of
    * using the JSON filter syntax by Loopback
    */
-  Biblio.query = function (q, from, size, cb) {
+  Biblio.query = function (q, from, size, scroll, cb) {
     var connector = Biblio.dataSource.connector
     var modelName = Biblio.definition.name;
     var idName = connector.idName(modelName)
 
-    console.log('Query: "' + q + '" idname: ' + idName, ' modelName: ' + modelName)
+    console.log(`Query: ${q}, idName: ${idName}, modelName: ${modelName}, from: ${from}, size: ${size}`)
 
-    connector.db.search({
+    var args = {
       index: 'okapi',
       q: q,
       from: from || 0,
-      size: size || 50,
-    }, function (error, response) {
+      size: size || 50
+    }
+
+    if (scroll) {
+      args.scroll = '1m'   // no need for any longer
+    }
+
+    connector.db.search(args, function (error, response) {
       if (error) console.error(error);
 
       var results = [];
@@ -29,12 +35,16 @@ module.exports = function(Biblio) {
           results.push(connector.dataSourceToModel(modelName, item, idName));
         });
       }
-      cb(error, {
+      var out = {
         total: total,
         data: results
-      });
+      }
+      if (response._scroll_id) {
+        out._scroll_id = response._scroll_id
+      }
+      cb(error, out);
     });
-  };
+  }
 
   Biblio.remoteMethod('query', {
     http: { verb: 'get' },
@@ -42,7 +52,8 @@ module.exports = function(Biblio) {
     accepts: [
       {arg: 'q', type: 'string'},
       {arg: 'from', type: 'integer'},
-      {arg: 'size', type: 'integer'}
+      {arg: 'size', type: 'integer', description: 'Result set size. If a set larger than 10000 is needed, use scroll.'},
+      {arg: 'scroll', type: 'boolean', description: 'Set to true to return a "scroll_id" that you can use with the scroll API to return large datasets. The returned ID is always valid for 1 minute.'},
     ],
     returns: [{
       root: true,
@@ -52,6 +63,56 @@ module.exports = function(Biblio) {
       arg: 'total',
       type: 'integer'
     }]
-  });
+  })
+
+  /**
+   * Interface to ElasticSearch Scroll Api
+   */
+  Biblio.scroll = function (scroll_id, cb) {
+    var connector = Biblio.dataSource.connector
+    var modelName = Biblio.definition.name
+    var idName = connector.idName(modelName)
+
+    connector.db.scroll({
+      scroll_id: scroll_id,
+      scroll: '1m',
+    }, function (error, response) {
+      if (error) console.error(error);
+
+      var results = [];
+      var total = 0;
+      console.log(response)
+      if (response.hits) {
+        total = response.hits.total;
+        response.hits.hits.forEach(function (item) {
+          results.push(connector.dataSourceToModel(modelName, item, idName));
+        });
+      }
+      var out = {
+        total: total,
+        data: results
+      }
+      if (response._scroll_id) {
+        out._scroll_id = response._scroll_id
+      }
+      cb(error, out);
+    });
+  }
+
+  Biblio.remoteMethod('scroll', {
+    http: { verb: 'get' },
+    description: 'Retrieve large dataset using the Elasticsearch scroll API',
+    accepts: [
+      {arg: 'scroll_id', type: 'string', description: 'A "scroll_id" that you get from a "query" response.'},
+    ],
+    returns: [{
+      root: true,
+      arg: 'data',
+      type: [Biblio.definition.name],
+    }, {
+      arg: 'total',
+      type: 'integer'
+    }]
+  })
 
 };
